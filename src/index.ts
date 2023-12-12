f18c2ea395c9a8854376abcde05dccbc5261815f
Niklas Gehlen <niklas@namespacelabs.com>
2023-12-13 00:45:46 +0100

    test input

--- src/index.ts
+++ src/index.ts
@@ -117,13 +117,8 @@ async function resolveCacheMode(cacheMode: string): Promise<Path[]> {
 
       const json = await getExecStdout(`pnpm m ls --depth -1 --json`);
       console.log(json);
-      const parsed = JSON.parse(json);
 
-      console.log(parsed);
-
-      const mapped = await getExecStdout(
-        `pnpm m ls --depth -1 --json | jq 'map(.path)'`
-      );
+      const mapped = await getExecStdout(`jq 'map(.path)'`, json);
       console.log(mapped);
 
       const pnpmModules = await getExecStdout(
